c301333a14e74bd4a685f37951eae8fd4c3309cf
Vishwas <Vishwas.p13@gmail.com>
2020-08-17 12:35:13 +0530

    Added Mono test cases

--- src/test/java/com/reactive/spring/FluxAndMonoTest.java
+++ src/test/java/com/reactive/spring/FluxAndMonoTest.java
@@ -3,6 +3,8 @@ package com.reactive.spring;
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -14,5 +16,33 @@ public class FluxAndMonoTest {
 				.concatWith(Flux.just("After Error"))
 				.log();
 		veryFlux.subscribe(System.out::println, (e) -> System.err.println("Code breaks "+e), () -> System.out.println("Completed"));
+	}
+	@Test
+	public void fluxTestElementsWithoutError() {
+		Flux<String> veryFlux = 
+				Flux.just("Spring","Spring Boot","Reactive Streams")
+				.log();
+		
+		StepVerifier.create(veryFlux)
+		.expectNext("Spring")
+		.expectNext("Spring Boot")
+		.expectNext("Reactive Streams")
+		.verifyComplete();
+		}
+	
+	@Test
+	public void monoTest() {
+		Mono<String> mono= Mono.just("Spring Boot");
+		StepVerifier.create(mono.log())
+		.expectNext("Spring Boot")
+		.verifyComplete();
+		
+	}
+	
+	@Test
+	public void monoTestError() {
+		StepVerifier.create(Mono.error(new RuntimeException("Exception Occurred")).log())
+		.expectError(RuntimeException.class)
+		.verify();
 	}
 }
